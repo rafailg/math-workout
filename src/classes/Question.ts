10e3657c4ea8e57ded6d1a8d0a294b1875426569
Rafail <dev@rfgs.me>
2023-07-01 11:55:17 +0200

    Initial Commit

---
+++ src/classes/Question.ts
@@ -0,0 +1,7 @@
+
+export class Question{
+    public Description:string
+    public Answer: number
+    public Solved: boolean
+    public CompletedIn: number
+}
---
+++ src/classes/Workout.ts
@@ -0,0 +1,6 @@
+import type { Question } from "./Question"
+
+export class Workout{
+    public Questions: Question[]
+    public CompletedAt: Date
+}
