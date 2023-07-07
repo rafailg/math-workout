e50c20ee43b34fbc7320b196c75437f9ce9b8f68
Rafail <dev@rfgs.me>
2023-07-07 09:42:35 +0200

    focus inputs

--- src/lib/Question.svelte
+++ src/lib/Question.svelte
@@ -1,28 +1,60 @@
 <script lang="ts">
+    import { onMount, afterUpdate } from "svelte";
     import { Question } from "../classes/Question";
     export let moveToNextQuestion = () => {}
 
     export let currentQuestion: Question = null;
     let userAnswer:string;
     let startTime: Date = new Date();
+    const delay = 1000
+    let inputElement: HTMLInputElement;
+    let disableInput = false
+    let solved = false;
+
+    afterUpdate(()=>{
+        inputElement?.focus()
+    })
 
     function checkAnswer(){
         if(!currentQuestion) return;
 
         if(userAnswer == currentQuestion.Answer.toString()){
-            alert("Correct answer!")
-            currentQuestion.Solved = true;
-            currentQuestion.CompletedIn = new Date().getSeconds() - startTime.getSeconds()
-            userAnswer = ""
-            moveToNextQuestion()
+            solved = true;
+            disableInput = true;
+
+            //Move to next question or end workout after a period of time.
+            setTimeout(()=>{
+                currentQuestion.Solved = true;
+                currentQuestion.CompletedIn = new Date().getSeconds() - startTime.getSeconds()
+                userAnswer = ""
+                disableInput = false;
+                solved = false;
+                moveToNextQuestion()
+            },delay)
         }
     }
-
 </script>
 
 <div>
     {#if currentQuestion}
-        <h2>{currentQuestion.Description}</h2>
-        <input type="text" bind:value={userAnswer} on:input={checkAnswer} inputmode="numeric">
+        <h2
+        class={solved?"solved":""}>
+            {currentQuestion.Description}
+        </h2>
+        
+        <input 
+        type="text" 
+        bind:this={inputElement}
+        bind:value={userAnswer} 
+        on:input={checkAnswer}
+        inputmode="numeric" 
+        disabled={solved} 
+        class={solved?"solved":""}>
     {/if}
 </div>
+
+<style>
+    .solved{
+        color: green;
+    }
+</style>
